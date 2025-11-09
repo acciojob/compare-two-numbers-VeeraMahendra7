5d10bd407a14d3f285e59801bc99d0a241b55b8a
VeeraMahendra7 <140475884+VeeraMahendra7@users.noreply.github.com>
2025-11-09 10:08:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+function compareTwo(n,m){
+	if(n === m) return true;
+	else false;
+}
+
+const n = parseInt(prompt("Enter number: "));
+const m = parseInt(prompt("Enter number: "));
+
+alert(compareTwo(n,m));
